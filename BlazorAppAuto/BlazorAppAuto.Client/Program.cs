02c928d2807f166731e857fa9aa3cf85431197e0
Yizheng Liu <yliu@atl.co.uk>
2026-01-15 14:07:46 +0000

    -.net8

--- BlazorAppAuto/BlazorAppAuto.Client/Program.cs
+++
@@ -1,6 +0,0 @@
-using BlazorAppAuto.Client;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Shared;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-await builder.Build().RunAsync();
--- BlazorAppAuto/BlazorAppAuto/Hubs/NotificationHub.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace BlazorAppAuto.Hubs;
-
-public class NotificationHub : Hub;

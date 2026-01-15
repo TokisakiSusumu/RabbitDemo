32c9b5f9d5a79792936807ed0ad5e54bb040c747
Yizheng Liu <yliu@atl.co.uk>
2026-01-15 13:32:43 +0000

    code chnage

--- BlazorAppAuto/BlazorAppAuto/Program.cs
+++ BlazorAppAuto/BlazorAppAuto/Program.cs
@@ -1,16 +1,33 @@
 using BlazorAppAuto.Client.Pages;
 using BlazorAppAuto.Components;
+using BlazorAppAuto.Hubs;
+using BlazorAppAuto.Services;
 using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<WarehouseBookingConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -18,17 +35,18 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+app.MapHub<NotificationHub>("/notificationhub");
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorAppAuto.Client._Imports).Assembly);
+
 app.Run();
---
+++ BlazorAppAuto/BlazorAppAuto/Services/WarehouseBookingConsumer.cs
@@ -0,0 +1,14 @@
+﻿using BlazorAppAuto.Hubs;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using Shared;
+
+namespace BlazorAppAuto.Services;
+
+public class WarehouseBookingConsumer(IHubContext<NotificationHub> hubContext) : IConsumer<WarehouseBookingDTO>
+{
+    public async Task Consume(ConsumeContext<WarehouseBookingDTO> context)
+    {
+        await hubContext.Clients.All.SendAsync("ReceiveMessage", context.Message);
+    }
+}
